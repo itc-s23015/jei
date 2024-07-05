3e9138d4b746b1d1d3bf07e3b1f1537f4c599205
itc-s23015 <s23015@std.it-college.ac.jp>
2024-07-05 12:19:08 +0900

    kadai

---
+++ d5Q27kadai.java
@@ -0,0 +1,25 @@
+// s23015
+
+class d5Q27kadai {
+    public static void main(String[] args) {
+	int control = 3;
+	String mode;
+	
+	switch (control) {
+	   case 1:
+	      mode = "warm";
+	      break;
+	   case 2:
+	      mode = "cool";
+	      break;
+	   case 3:
+	      mode = "wind";
+	      break;
+	   default:
+	      mode = "not found";
+	      break;
+	}
+	System.out.print(mode);	
+    }
+}
+		 
