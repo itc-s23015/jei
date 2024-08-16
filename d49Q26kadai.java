dbafba30d7707c612d39ba5a6facadd45e12c79b
itc-s23015 <s23015@std.it-college.ac.jp>
2024-08-16 10:21:58 +0900

    kadai

---
+++ d49Q26kadai.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// s23015
+
+public class d49Q26kadai {
+	public static void main(String[] args) {
+		d49Q26kadai obj = new d49Q26kadai();
+		obj.show(args[0]);
+	}
+	
+	void show(String n) {
+		System.out.print("value is " + n);
+	}
+}
